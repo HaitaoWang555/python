16a914183e0ba5abe84ccf10f4151bd3ef081245
wanghaitao <15124505701@163.com>
2018-08-08 15:04:59 +0800

    登录注册

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # 解决循环导入问题
 from app import routes, models
