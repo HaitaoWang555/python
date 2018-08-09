cdb189c9fad52487aa85f713fea7d9842c1e4fac
wanghaitao <15124505701@163.com>
2018-08-09 09:26:54 +0800

    记录日志到文件中

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +13,21 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('app startup')
+
 
 # 解决循环导入问题
 from app import routes, models, errors
