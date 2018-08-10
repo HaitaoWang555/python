1a8e82140373f06cc4fe1f9062961341421fc2e9
wanghaitao <15124505701@163.com>
2018-08-10 09:17:21 +0800

    邮件支持

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from flask_mail import Mail
 from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 
 if not app.debug:
